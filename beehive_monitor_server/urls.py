9c8677bd13bb2fe6a91349d0dd8c17c6e56b4043
Marco Roose <marco.roose@gmx.de>
2017-12-28 15:14:53 +0100

    added a favicon

--- beehive_monitor_server/urls.py
+++ beehive_monitor_server/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import (
+    TemplateView,
+    RedirectView
+)
 
 urlpatterns = [
+    url(r"favicon.ico", RedirectView.as_view(url="static/icons/favicon.ico")),
     url(r"collector/", include("DataCollector.urls")),
     url(
         regex='admin/',
